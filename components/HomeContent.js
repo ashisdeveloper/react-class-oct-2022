5b0f9f87522fd26f452ef8a6b1f7ec026db3980c
Ashis <srimanashis@gmail.com>
2022-10-26 13:20:05 +0530

    Component props

--- components/HomeContent.js
+++ components/HomeContent.js
@@ -1,7 +1,46 @@
+const boxes = [
+	{ img: "/images/1652163564.jpg", title: 'Himachal Pradesh' },
+	{ img: "/images/1652163616.jpg", title: 'Manali' },
+	{ img: "/images/1652163630.jpg", title: 'Shimla' },
+	{ img: "/images/1652163644.jpg", title: 'Kerla' },
+]
+
+const RenderBox = ({ img, title }) => {
+	return <>
+		<div className="box">
+			<img src={img} alt="" />
+			<div className="title p-3">{title}</div>
+		</div>
+	</>
+}
+
 const HomeContent = () => {
 	return <>
 		<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat perspiciatis iusto laudantium deserunt nam facere odio a laboriosam quod, ducimus dolorem obcaecati facilis, atque ipsam amet earum, neque ipsum fugit?</p>
 
+		<div className="d-flex flex-wrap box-content" style={{ gap: '20px' }}>
+			{/* 3rd Way */}
+			{boxes.map((item, index) => <RenderBox key={index} img={item.img} title={item.title} />)}
+
+			{/* 2nd Way */}
+			{/* {boxes.map((item, index) => <div key={index} className="box">
+				<img src={item.img} alt="" />
+				<div className="title">{item.title}</div>
+			</div>)} */}
+			{/* 1st Way */}
+			{/* <div className="box">
+				<img src="/images/1652163564.jpg" alt="" />
+				<div className="title">Himachal Pradesh</div>
+			</div>
+			<div className="box">
+				<img src="/images/1652163616.jpg" alt="" />
+				<div className="title">Manali</div>
+			</div>
+			<div className="box">
+				<img src="/images/1652163630.jpg" alt="" />
+				<div className="title">Shimla</div>
+			</div> */}
+		</div>
 
 	</>
 };
--- components/StaticComponents.js
+++ components/StaticComponents.js
@@ -3,10 +3,11 @@ import Link from "next/link";
 export const Header = () => {
 	return (
 		<>
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				{/* <a className="navbar-brand" href="#">Navbar</a> */}
-				<Link href={"/"} passHref><a className="navbar-brand">My App</a></Link>
-				<button className="navbar-toggler" type="button" dataToggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" ariaExpanded="false" ariaLabel="Toggle navigation">
+			<nav className="navbar navbar-expand-lg navbar-light bg-light px-0">
+				<Link href={"/"} passHref><a className="navbar-brand">
+					<img src="/images/logo.png" alt="Logo" style={{width: '200px'}}/>
+				</a></Link>
+				<button className="navbar-toggler" type="button">
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
@@ -20,32 +21,9 @@ export const Header = () => {
 						<li className="nav-item">
 							<Link href={"/contact"}><a className="nav-link">Contact</a></Link>
 						</li>
-						{/* <li className="nav-item active">
-							<a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">Features</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">Pricing</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link disabled">Disabled</a>
-						</li> */}
 					</ul>
 				</div>
 			</nav>
-			{/* <ul>
-				<li>
-					<Link href={"/"}>Home</Link>
-				</li>
-				<li>
-					<Link href={"/services"}>Services</Link>
-				</li>
-				<li>
-					<Link href={"/contact"}>Contact</Link>
-				</li>
-			</ul> */}
 		</>
 	);
 };
